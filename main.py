73bd215f85f51b61ccea385c59b46b929f86edc6
dima <dogima1@mail.ru>
2022-10-21 21:08:14 +0300

    easy calculator

--- main.py
+++ main.py
@@ -3,9 +3,21 @@ def calc(znak, a, b):
         return a + b
     elif (znak == '-'):
         return a - b
+    elif (znak == '*'):
+        return a * b
+    elif (znak == '/'):
+        if (znak == '/'):
+             choice = input("Вам нужно целочисленное деление? (напишите + или -)" )
+             if (choice == '+'):
+                 return a // b
+             else:
+                 return a / b
     return "ОШИБКА"
 
 znak = input("Введите знак -> ")
 a = int(input("Введите первое число = "))
 b = int(input("Введите второе число = "))
+
+
+
 print(calc(znak, a, b))
