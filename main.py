00a84ee4a502c58ab7f2718b61c4d66a83d71f72
dima <dogima1@mail.ru>
2022-10-21 20:57:25 +0300

    add error

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
-def calc(a, b):
-    return a + b
+def calc(znak, a, b):
+    if (znak == '+'):
+        return a + b
+    return "ОШИБКА"
 
+znak = input("Введите знак -> ")
 a = int(input("Введите первое число = "))
 b = int(input("Введите второе число = "))
-print(calc(a, b))
+print(calc(znak, a, b))
